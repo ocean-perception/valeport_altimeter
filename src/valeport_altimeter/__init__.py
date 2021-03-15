46c23fd10ab06081f3a8fead06cb45eeb6821797
Miquel Massot <miquel.massot@gmail.com>
2021-03-15 18:07:11 +0000

    Remove old files

--- src/valeport_altimeter/__init__.py
+++
@@ -1,5 +0,0 @@
-from .commands import Command, Message  # noqa
-from .replies import Reply, Error  # noqa
-from .socket import Socket  # noqa
-from .valeport_altimeter import VA500  # noqa
-from .valeport_altimeter import main  # noqa
